238879cb48abea69eab59b03e759c786ac19df7b
youaregod666 <94928206+youaregod666@users.noreply.github.com>
2022-02-18 15:48:49 +0100

    Update Main.lua

--- Installer/Main.lua
+++ Installer/Main.lua
@@ -42,7 +42,7 @@ end
 
 local function title()
 	local y = math.floor(screenHeight / 2 - 1)
-	centrizedText(y, 0x2D2D2D, "Starting Mine Setup")
+	centrizedText(y, 0x2D2D2D, "Starting IMineOS Setup")
 
 	return y + 2
 end
@@ -235,7 +235,7 @@ window:addChild(GUI.panel(1, 1, window.width, window.height, 0xE1E1E1))
 
 -- Top menu
 local menu = workspace:addChild(GUI.menu(1, 1, workspace.width, 0xF0F0F0, 0x787878, 0x3366CC, 0xE1E1E1))
-local installerMenu = menu:addContextMenuItem("Mine", 0x2D2D2D)
+local installerMenu = menu:addContextMenuItem("IMineOS", 0x2D2D2D)
 installerMenu:addItem("Shutdown").onTouch = function()
 	computer.shutdown()
 end
@@ -296,14 +296,14 @@ local localization
 local stage = 1
 local stages = {}
 
-local usernameInput = newInput("")
+local usernameInput = newInput("IMineOS")
 local passwordInput = newInput("", false, "•")
 local passwordSubmitInput = newInput("", false, "•")
 local usernamePasswordText = GUI.text(1, 1, 0xCC0040, "")
 local passwordSwitchAndLabel = newSwitchAndLabel(26, 0x66DB80, "", false)
 
 local wallpapersSwitchAndLabel = newSwitchAndLabel(30, 0xFF4980, "", true)
-local screensaversSwitchAndLabel = newSwitchAndLabel(30, 0xFFB600, "", true)
+local screensaversSwitchAndLabel = newSwitchAndLabel(30, 0xFFB600, "", false)
 local applicationsSwitchAndLabel = newSwitchAndLabel(30, 0x33DB80, "", true)
 local localizationsSwitchAndLabel = newSwitchAndLabel(30, 0x33B6FF, "", true)
 
